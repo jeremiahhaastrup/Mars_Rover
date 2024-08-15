6d4c1353321d39e372bd6450deddd6632faedc68
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-15 19:07:16 +0100

    feat: parse for plateau size input to enum types

--- src/main/java/org/example/input/PlateauParser.java
+++ src/main/java/org/example/input/PlateauParser.java
@@ -1,4 +1,52 @@
-package input;
+package org.example.input;
+
+import org.example.Plateau;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class PlateauParser {
+    public ArrayList<Plateau> plateauInput() {
+
+        ArrayList<Plateau> result = new ArrayList<>();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            try {
+
+                System.out.println("Enter The Measurements For The Plateau.");
+                System.out.println("The Plateau Size Has To Be Equal Or More Than 2 For X and Y");
+                System.out.println("X: Width");
+                System.out.println("Y: Length");
+
+                int enteredXPoint = scanner.nextInt();
+                int enteredYPoint = scanner.nextInt();
+
+                if (enteredXPoint < 2 || enteredYPoint < 2) {
+                    throw new IllegalArgumentException();
+                }
+
+
+                String enteredCompassPoints = Integer.toString(enteredXPoint,enteredYPoint);
+
+                for (char index : enteredCompassPoints.toCharArray()) {
+                    if (enteredCompassPoints.indexOf(index) == 0) {
+                        result.add(Plateau.X);
+                    }
+                    if (enteredCompassPoints.indexOf(index) == 1) {
+                        result.add(Plateau.Y);
+                    }
+                }
+
+                scanner.close();
+                break;
+
+            } catch (InputMismatchException e) {
+                System.err.println("Incorrect Format Houston! Try Again");
+
+            }
+        }
+        return result;
+    }
 }
