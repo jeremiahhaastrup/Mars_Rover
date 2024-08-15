b349e0bbf935592a884867d0f6e6d6ddc81c1e8a
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-15 19:06:25 +0100

    feat: parse for instruction inputs to enum types

--- src/main/java/org/example/input/InstructionParser.java
+++ src/main/java/org/example/input/InstructionParser.java
@@ -2,15 +2,17 @@ package org.example.input;
 
 import org.example.Instruction;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InstructionParser {
-    public void instructionInput() {
+    public ArrayList<Instruction> instructionInput() {
+        ArrayList<Instruction> result = new ArrayList<>();
+
         while(true) {
             try {
                 Scanner scanner = new Scanner(System.in);
-                StringBuilder result = new StringBuilder();
 
                 System.out.println("Enter The Instructions For The Rover.");
                 System.out.println("R: Spin Right at 90 degrees.");
@@ -19,26 +21,24 @@ public class InstructionParser {
                 String enteredInstruction = scanner.nextLine();
 
                 for (char index : enteredInstruction.toCharArray()) {
-                    switch(index) {
-                        case 'R':
-                            result.append(Instruction.R);
-                        break;
-                        case 'L':
-                            result.append(Instruction.L);
-                        break;
-                        case 'M':
-                            result.append(Instruction.M);
-                        break;
-                        default:
-                            System.err.println(index + " - Incorrect Format Houston. Try again");
+                    if (index == 'R') {
+                        result.add(Instruction.R);
+                    } else if (index == 'L') {
+                        result.add(Instruction.L);
+                    } else if (index == 'M') {
+                        result.add(Instruction.M);
+                    } else {
+                        throw new InputMismatchException();
                     }
                 }
-                System.out.println(result);
+
                 scanner.close();
                 break;
+
             } catch (InputMismatchException e) {
-                System.out.println("Incorrect Format Houston. Try Again" + e);
+                System.out.println("Incorrect Format Houston. Try Again " + e);
             }
         }
+        return result;
     }
 }
