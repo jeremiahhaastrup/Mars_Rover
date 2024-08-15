dfb77d0d058ca8e1c5f849cbecff092748ed7bb6
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-15 15:45:26 +0100

    feat: additional of I/O feature for instructions

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import org.example.input.InstructionParser;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        InstructionParser instructionParser = new InstructionParser();
+        instructionParser.instructionInput();
     }
 }
---
+++ src/main/java/org/example/input/InstructionParser.java
@@ -0,0 +1,37 @@
+package input;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InstructionParser {
+    public void instructionInput() {
+        while(true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Enter The Instructions For The Rover.");
+                System.out.println("R: Spin Right at 90 degrees.");
+                System.out.println("L: Spin Left at 90 degrees.");
+                System.out.println("M: Move Forward by One Grid Point");
+                String enteredInstruction = scanner.nextLine();
+                for (char index : enteredInstruction.toCharArray()) {
+                    switch(index) {
+                        case 'R':
+                            System.out.println("Spin to the Right");
+                        break;
+                        case 'L':
+                            System.out.println("Spin to the Left");
+                        break;
+                        case 'M': System.out.println("Move One Grid Point");
+                        break;
+                        default:
+                            System.err.println("Incorrect Format Houston. Try again");
+                    }
+                }
+                scanner.close();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Incorrect Format Houston. Try Again" + e);
+            }
+        }
+    }
+}
