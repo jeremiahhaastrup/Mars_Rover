8d19fea0aa023e8e0cc81f826c88260edfebaa0b
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-15 12:22:34 +0100

    feat: new enum and class for the plateau size for the rover movements

---
+++ src/main/java/org/example/Plateau.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public enum Plateau {
+    X,
+    Y
+}
---
+++ src/main/java/org/example/PlateauSize.java
@@ -0,0 +1,6 @@
+package org.example;
+
+public class PlateauSize {
+    private Plateau width = Plateau.X;
+    private Plateau length = Plateau.Y;
+}
