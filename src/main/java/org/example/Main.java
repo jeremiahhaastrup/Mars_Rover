0f7b9e7c955cf1982daff5acb0b2f94cab8553a1
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-15 19:08:12 +0100

    feat: declaring and examining i/o process in console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,20 @@
 package org.example;
 
+import org.example.input.CompassParser;
 import org.example.input.InstructionParser;
+import org.example.input.PlateauParser;
 
 public class Main {
     public static void main(String[] args) {
+
         InstructionParser instructionParser = new InstructionParser();
         instructionParser.instructionInput();
+
+//        PlateauParser plateauParser = new PlateauParser();
+//        plateauParser.plateauInput();
+
+
+//        CompassParser compassParser = new CompassParser();
+//        compassParser.compassInput();
     }
 }
