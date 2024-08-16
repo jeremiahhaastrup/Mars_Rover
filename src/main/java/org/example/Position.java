183b184d2f7edbff110afab4b31ed829e869ad28
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-16 14:21:59 +0100

    feat: addition of constructor and getters for the position of the rover

--- src/main/java/org/example/Position.java
+++ src/main/java/org/example/Position.java
@@ -4,4 +4,22 @@ public class Position {
     private int x;
     private int y;
     private Compass facing;
+
+    public Position(int x, int y, Compass facing) {
+        this.x = x;
+        this.y = y;
+        this.facing = facing;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Compass getFacing() {
+        return facing;
+    }
 }
