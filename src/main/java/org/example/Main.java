b3e0a078138f3c625dde5d9f66254c6c88516393
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-16 14:21:18 +0100

    feat: declaring and instansiating scanner methods for user input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,18 +3,20 @@ package org.example;
 import org.example.input.CompassParser;
 import org.example.input.InstructionParser;
 import org.example.input.PlateauParser;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        InstructionParser instructionParser = new InstructionParser();
+        PlateauParser plateauParser = new PlateauParser(scanner);
+        CompassParser compassParser = new CompassParser(scanner);
+        InstructionParser instructionParser = new InstructionParser(scanner);
+
+        plateauParser.plateauInput();
+        compassParser.compassInput();
         instructionParser.instructionInput();
 
-//        PlateauParser plateauParser = new PlateauParser();
-//        plateauParser.plateauInput();
-
-
-//        CompassParser compassParser = new CompassParser();
-//        compassParser.compassInput();
+        scanner.close();
     }
 }
