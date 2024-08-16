783410743871660ce485e1c5377d24d3d6c2d91f
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-16 14:20:14 +0100

    feat: addition of scanner input for PlateauParser class

--- src/main/java/org/example/input/PlateauParser.java
+++ src/main/java/org/example/input/PlateauParser.java
@@ -5,22 +5,23 @@ import org.example.Plateau;
 import java.util.*;
 
 public class PlateauParser {
+
+    HashMap<Plateau, Integer> result;
+    private Scanner scanner;
+
+    public PlateauParser(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
     public Map<Plateau, Integer> plateauInput() {
 
-        Scanner scanner = new Scanner(System.in);
-
-        HashMap<Plateau, Integer> result;
         while (true) {
             try {
                 result = new HashMap();
 
-                System.out.println("Enter The Measurements For The Plateau.");
-                System.out.println("The Plateau Size Has To Be Equal Or More Than 2 For X and Y");
-                System.out.println("X: Width");
-                System.out.println("Y: Length");
-
                 int enteredXPoint = scanner.nextInt();
                 int enteredYPoint = scanner.nextInt();
+                scanner.nextLine();
 
                 if (enteredXPoint < 2 || enteredYPoint < 2) {
                     throw new IllegalArgumentException();
@@ -29,12 +30,11 @@ public class PlateauParser {
                 result.put(Plateau.X, enteredXPoint);
                 result.put(Plateau.Y, enteredYPoint);
 
-                System.out.println(result);
-                scanner.close();
                 break;
 
             } catch (InputMismatchException e) {
-                System.err.println("Incorrect Format Houston! Try Again");
+                System.err.println("Incorrect Format Houston! Try Again.");
+                scanner.nextLine();
             }
         }
         return result;
