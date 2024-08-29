142ee5fef7c58efafc1b3005f3d8b325dd8f6b91
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-29 21:33:46 +0100

    refactor: removed redundant code and comments from instructionParser

--- src/main/java/org/example/input/InstructionParser.java
+++ src/main/java/org/example/input/InstructionParser.java
@@ -33,36 +33,3 @@ public class InstructionParser {
 
     }
 }
-//        while(true) {
-//            try {
-//
-//                System.out.println("Please Provide The Rover With Instructions.");
-//                System.out.println("Key:");
-//                System.out.println("L -> Spins the Rover 90 degrees Left without moving from the current coordinate point.");
-//                System.out.println("R -> Spins the Rover 90 degrees Right without moving from the current coordinate point");
-//                System.out.println("M -> Moves the Rover forward by one grid point, maintaining the same heading/orientation");
-//
-//                String enteredInstruction = scanner.nextLine();
-//
-//                for (char index : enteredInstruction.toCharArray()) {
-//                    if (index == 'R' || index == 'r') {
-//                        result.add(Instruction.R);
-//                    } else if (index == 'L' || index == 'l') {
-//                        result.add(Instruction.L);
-//                    } else if (index == 'M' || index == 'm') {
-//                        result.add(Instruction.M);
-//                    } else {
-//                        throw new InputMismatchException();
-//                    }
-//                }
-//                break;
-//            } catch (InputMismatchException e) {
-//                System.out.println("Incorrect Instructions Houston! Try Again.");
-//            } catch (NoSuchElementException e) {
-//                System.out.println("No Input Provided. Please Provide The Rover With Instructions.");
-//                scanner.nextLine();
-//            }
-//        }
-//        return result;
-//    }
-//}
