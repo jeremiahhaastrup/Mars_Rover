3fe8ca228cec171f5a646aa72d31632d68e2be93
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-29 21:33:23 +0100

    refactor: removed redundant code and comments

--- src/main/java/org/example/input/CompassParser.java
+++ src/main/java/org/example/input/CompassParser.java
@@ -2,8 +2,6 @@ package org.example.input;
 
 import org.example.logic.Position;
 import org.example.type.Compass;
-import org.example.type.Instruction;
-import org.example.type.PlateauSize;
 
 import java.util.*;
 
@@ -38,57 +36,3 @@ public class CompassParser {
     }
 }
 
-//        while(true) {
-//            try {
-//                result = new HashMap<>();
-//
-//                if (!scanner.hasNextLine()) {
-//                    System.out.println("No Input Provided. Please Enter Two Integers (X and Y Coordinates) and Direction.");
-//                    continue;
-//                }
-//
-//                System.out.println("Please Enter Two Integers (X and Y Coordinates) and a Direction For The Rovers Starting Position:");
-//                System.out.println("Key:");
-//                System.out.println("W -> West");
-//                System.out.println("E -> East");
-//                System.out.println("N -> North");
-//                System.out.println("S -> South");
-//                int xCoordinate = scanner.nextInt();
-//                int yCoordinate = scanner.nextInt();
-//                char enteredCompassPoint = scanner.next().toUpperCase().charAt(0);
-//
-//                if (xCoordinate < 2 || yCoordinate < 2) {
-//                    System.out.println("Please Enter Two Integers (X and Y Coordinates) that are more than 2");
-//                    continue;
-//                }
-//
-//                if (xCoordinate >= plateauParser.result.get(PlateauSize.X)
-//                        || yCoordinate >= plateauParser.result.get(PlateauSize.Y)) {
-//                    System.out.println("Please Enter Two Integers (X and Y Coordinates) less than the plateau coordinates.");
-//                    continue;
-//                }
-//
-//                Compass compassDirection = switch (enteredCompassPoint) {
-//                    case 'W' -> Compass.W;
-//                    case 'E' -> Compass.E;
-//                    case 'N' -> Compass.N;
-//                    case 'S' -> Compass.S;
-//                    default -> throw new InputMismatchException();
-//                };
-//
-//                Position position = new Position(xCoordinate, yCoordinate, compassDirection);
-//                result.put(compassDirection, position);
-//                scanner.nextLine();
-//                break;
-//
-//            } catch (InputMismatchException | IllegalArgumentException e) {
-//                System.out.println("Incorrect Compass Coordinates Houston!! Please Try Again.");
-//                scanner.nextLine();
-//            } catch (NoSuchElementException e) {
-//                System.out.println("No Input Provided. Please Enter Two Integers (X and Y Coordinates) and Direction.");
-//                scanner.nextLine();
-//            }
-//        }
-//        return result;
-//    }
-//}
