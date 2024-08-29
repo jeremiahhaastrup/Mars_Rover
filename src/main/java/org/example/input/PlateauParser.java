dbc24fc453af03ea299173388d784b98429b2147
Jeremiah Haastrup <jeremiahhaastrup1@gmail.com>
2024-08-29 21:34:03 +0100

    refactor: removed redundant code and comments from plateauParser

--- src/main/java/org/example/input/PlateauParser.java
+++ src/main/java/org/example/input/PlateauParser.java
@@ -26,35 +26,3 @@ public class PlateauParser {
         return result;
     }
 }
-
-//        while (true) {
-//            try {
-//                result = new HashMap<>();
-//
-//                System.out.println("Please Provide The Plateau Size With Two Integers");
-//                System.out.println("First Integer: Width");
-//                System.out.println("Second Integer: Height");
-//                int enteredXPoint = scanner.nextInt();
-//                int enteredYPoint = scanner.nextInt();
-//
-//
-//                if (enteredXPoint < 2 || enteredYPoint < 2) {
-//                    throw new IllegalArgumentException();
-//                }
-//
-//                result.put(PlateauSize.X, enteredXPoint);
-//                result.put(PlateauSize.Y, enteredYPoint);
-//                System.out.println(result);
-//                break;
-//
-//            } catch (InputMismatchException | IllegalArgumentException e) {
-//                System.out.println("Incorrect Format Houston! Try Again.");
-//                scanner.nextLine();
-//            } catch (NoSuchElementException e) {
-//                System.out.println("No Input Provided. Please Provide The Plateau Size With Two Integers " + e);
-//                scanner.nextLine();
-//            }
-//        }
-//        return result;
-//    }
-
